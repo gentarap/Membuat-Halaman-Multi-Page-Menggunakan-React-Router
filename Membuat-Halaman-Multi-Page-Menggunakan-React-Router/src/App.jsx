334578323786891b7b955f69388e331a865e70f5
Gentar <gentarasmara@gmail.com>
2025-06-24 17:16:19 +0700

    Fix: app.jsx

--- Membuat-Halaman-Multi-Page-Menggunakan-React-Router/src/App.jsx
+++ Membuat-Halaman-Multi-Page-Menggunakan-React-Router/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/portfolio" element={<Portofolio />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
       </div>
       <Footer />
